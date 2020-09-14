03357bcf7be5d2e932c4e0711d655f328a633af7
GianiNavette <you@example.com>
2020-09-14 16:35:12 +0200

    Progression sur les titres des reglages

--- simu.js
+++ simu.js
@@ -1,6 +1,10 @@
 var a = 0;
 var b = 0;
 var c = 0;
+var nbh1=0;
+var nbh2=0;
+var nbp=0;
+
 var sideBarState=true;
 function myFunction() {
     c=c+1;
@@ -9,19 +13,35 @@ function myFunction() {
     var text = e.options[e.selectedIndex].text;
     var d1 = document.getElementById('myList');
 
-if ( value3 == "img") {
-  b = b + 1;
-  d1.insertAdjacentHTML('beforebegin', '<'+value3+' onclick="uploadImage('+b+')"; src="placeholder.jpg" id="image'+b+'"><'+value3+'/>');
-  document.getElementById('myList2').insertAdjacentHTML('beforebegin', '<button id="buttonObjet'+c+'" onclick="hideOtherEl(this,'+c+')">Image '+b+' ˅</button><div id="objet'+c+'" style="display:block;"><input id="inputimage'+b+'" type="file" onchange="readURL(this,'+b+');" /><button onClick="myFunction8('+b+')" >Petite</button><button onClick="myFunction9('+b+')" >Moyenne</button><button onClick="myFunction10('+b+')" >Grande</button></div></div>');
-} else {
-  a = a + 1;
-  d1.insertAdjacentHTML('beforebegin', '<'+value3+' onclick="clickSpecificbutton('+b+')" contenteditable="true" id="'+a+'">Cliquer pour modifier<'+value3+'/>');
-  
-  
-  
-  document.getElementById('myList2').insertAdjacentHTML('beforebegin', '<button id="buttonObjet'+c+'" onclick="hideOtherEl(this,'+c+')" >Texte '+a+' ˅</button><div id="objet'+c+'" style="display:block;"><label>Couleur du texte </label><input type="color" id="color'+a+'" name="head" onChange="myFunction2('+a+')" value="#000000"><button onClick="myFunction5('+a+')" >Gauche</button><button onClick="myFunction6('+a+')" >Centrer</button><button onClick="myFunction7('+a+')" >Droite</button></div></div>');
-  
-}
+    switch (value3) {
+        case 'img':
+            b = b + 1;
+            d1.insertAdjacentHTML('beforebegin', '<'+value3+' onclick="uploadImage('+b+','+c+')"; src="placeholder.jpg" id="image'+b+'"><'+value3+'/>');
+            document.getElementById('myList2').insertAdjacentHTML('beforebegin', '<button id="buttonObjet'+c+'" onclick="hideOtherEl(this,'+c+',true)">Image '+b+' ˅</button><div id="objet'+c+'" style="display:block;"><input id="inputimage'+b+'" type="file" onchange="readURL(this,'+b+');" /><button onClick="myFunction8('+b+')" >Petite</button><button onClick="myFunction9('+b+')" >Moyenne</button><button onClick="myFunction10('+b+')" >Grande</button></div></div>');        
+          break;
+        case 'h1':
+            nbh1=nbh1+1;
+            a = a + 1;
+            d1.insertAdjacentHTML('beforebegin', '<'+value3+' class="title" data-placeholder="Texte" onclick="clickSpecificbutton('+c+')" contenteditable="true" id="'+a+'"><'+value3+'/>');
+            document.getElementById('myList2').insertAdjacentHTML('beforebegin', '<button id="buttonObjet'+c+'" onclick="hideOtherEl(this,'+c+',true)" >'+text+' '+nbh1+' ˅</button><div id="objet'+c+'" style="display:block;"><label>Couleur du texte </label><input type="color" id="color'+a+'" name="head" onChange="myFunction2('+a+')" value="#000000"><button onClick="myFunction5('+a+')" >Gauche</button><button onClick="myFunction6('+a+')" >Centrer</button><button onClick="myFunction7('+a+')" >Droite</button></div></div>');
+        break;
+          case 'h2':
+            nbh2=nbh2+1;
+            a = a + 1;
+            d1.insertAdjacentHTML('beforebegin', '<'+value3+' class="title" data-placeholder="Texte" onclick="clickSpecificbutton('+c+')" contenteditable="true" id="'+a+'"><'+value3+'/>');
+            document.getElementById('myList2').insertAdjacentHTML('beforebegin', '<button id="buttonObjet'+c+'" onclick="hideOtherEl(this,'+c+',true)" >'+text+' '+nbh2+' ˅</button><div id="objet'+c+'" style="display:block;"><label>Couleur du texte </label><input type="color" id="color'+a+'" name="head" onChange="myFunction2('+a+')" value="#000000"><button onClick="myFunction5('+a+')" >Gauche</button><button onClick="myFunction6('+a+')" >Centrer</button><button onClick="myFunction7('+a+')" >Droite</button></div></div>');
+          break;
+        case 'p':
+            nbp=nbp+1;
+            a = a + 1;
+            d1.insertAdjacentHTML('beforebegin', '<'+value3+' class="title" data-placeholder="Texte" onclick="clickSpecificbutton('+c+')" contenteditable="true" id="'+a+'"><'+value3+'/>');
+            document.getElementById('myList2').insertAdjacentHTML('beforebegin', '<button id="buttonObjet'+c+'" onclick="hideOtherEl(this,'+c+',true)" >'+text+' '+nbp+' ˅</button><div id="objet'+c+'" style="display:block;"><label>Couleur du texte </label><input type="color" id="color'+a+'" name="head" onChange="myFunction2('+a+')" value="#000000"><button onClick="myFunction5('+a+')" >Gauche</button><button onClick="myFunction6('+a+')" >Centrer</button><button onClick="myFunction7('+a+')" >Droite</button></div></div>');
+          break;
+        default:
+          console.log(`Sorry, we are out of ${expr}.`);
+      }
+
+
 }
 
 function myFunction2(var1) {
@@ -72,11 +92,14 @@ function readURL(input,imageid) {
         reader.readAsDataURL(input.files[0]);
     }
 }
-function uploadImage(var1) { 
+function uploadImage(var1,objetId) { 
+    clickSpecificbutton(objetId);
     document.getElementById("inputimage" + ""+var1+"").click(); 
 } 
-clickSpecificbutton(objetId){
-    hideOtherEl("buttonObjet" + ""+objetId+"",objetId);
+function clickSpecificbutton(objetId)
+{
+    x = document.getElementById("buttonObjet" + ""+objetId+"")
+    hideOtherEl(x,objetId,false);
     
 }
 function sidebutton(x){
@@ -96,18 +119,22 @@ function sidebutton(x){
     x.classList.toggle("change");
 }
 
-function hideOtherEl(buttontext,objetId) {
+function hideOtherEl(buttontext,objetId,bool) {
 
     let str = buttontext.innerHTML
     str = str.slice(0, -1); 
     
     var x = document.getElementById("objet" + ""+objetId+"");
   if (x.style.display === "none") {
+    
     x.style.display = "block";
     buttontext.innerHTML = str +"˅";
   } else {
-    x.style.display = "none";
-    buttontext.innerHTML = str +"˄";
+    if (bool==true){
+        x.style.display = "none";
+        buttontext.innerHTML = str +"˄";
+    }  
+    
   }
 
 }
